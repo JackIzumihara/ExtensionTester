f034e2e9c9e665b1eae6f56cdea84a7270a48868
JackIzumihara <1551500700@k12.hi.us>
2026-05-05 18:51:33 +0000

    try now w/new test button

--- extensionTest.zip/scripts/content.js
+++ extensionTest.zip/scripts/content.js
@@ -1,6 +1,12 @@
 let annoyingAdPanel = document.getElementById('panels');
 let volume = document.getElementById('');
 
+const buttontest = document.createElement('p');
+buttontest.innerText = "click me click me click me click me click me ";
+document.body.appendChild(buttontest);
+buttontest.style.left = 10;
+buttontest.style.top = 10;
+
 function removeAdPanelTest(){
     annoyingAdPanel.style.display = "none";
     console.log("panel maybe removed");
