102cd1d3be8f4622afae9b1d6b28c6547444b2d1
JackIzumihara <1551500700@k12.hi.us>
2026-05-11 18:12:25 +0000

    maybe its fixed now

--- extensionTest.zip/scripts/content.js
+++ extensionTest.zip/scripts/content.js
@@ -5,6 +5,9 @@ let toggleVar = 1;
 function createTest(){
     alert("Extension has loaded successfully");
 }
+let createTestToggle = 0;
+let removeAdPanelToggle = 0;
+let adActiveTest = 0;
 
 function toggleEverything(){
     if(toggleVar == 1){
@@ -19,10 +22,6 @@ function toggleEverything(){
         adActiveTest = setInterval(recieveAdActive, 1);
     }
 }
-
-let createTestToggle = setTimeout(createTest, 5000);
-let removeAdPanelToggle = setInterval(removeAdPanelTest, 10000);
-let adActiveTest = setInterval(recieveAdActive, 1);
 
 function removeAdPanelTest(){
     annoyingAdPanel.style.display = "none";
