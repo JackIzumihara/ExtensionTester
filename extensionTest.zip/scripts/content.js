38604df1cd6f989d2bf7af76ada92c9929d42458
JackIzumihara <1551500700@k12.hi.us>
2026-05-14 07:06:37 +0000

    maybe it does something now

--- extensionTest.zip/scripts/content.js
+++ extensionTest.zip/scripts/content.js
@@ -1,13 +1,17 @@
+// really good video https://www.youtube.com/watch?v=mgFo3fxuUyA
+
 let annoyingAdPanel = document.getElementById('panels');
 let progressBar = document.getElementsByClassName('ytp-scrubber-button ytp-swatch-background-color');
 let toggleVar = 1;
 
 function createTest(){
-    alert("Extension has loaded successfully");
+    const site = window.location.hostname;
+    alert("maybe worked: " + site);
 }
-let createTestToggle = setTimeout(createTest, Infinity);
-let removeAdPanelToggle = setInterval(removeAdPanelTest, Infinity);
+
+let createTestToggle = setTimeout(createTest, 5000);
 let adActiveTest = setInterval(recieveAdActive, 1);
+let adSkipper = setInterval(skipAd, 1);
 
     var videoElement = document.querySelector("video"); //this code was found on https://www.matthewgatland.com/journal/2017-11-24-how-to-make-youtube-louder/, it will also be credited in the presentation
     var audioCtx = new AudioContext();
@@ -24,25 +28,33 @@ function unmute(){
     gainNode.gain.value = 1;
 }
 
-function toggleEverything(){
-    if(toggleVar == 1){
-        toggleVar = 0;
-        createTestToggle = setTimeout(createTest, 5000);
-        removeAdPanelToggle = setInterval(removeAdPanelTest, 10000);
-        adActiveTest = setInterval(recieveAdActive, 1);
-    }else{
-        clearInterval(adActiveTest);
-        clearInterval(removeAdPanelToggle);
-        clearInterval(createTestToggle);
-        toggleVar = 1;
-    }
-}
-
 function recieveAdActive(){
         if (document.querySelector("div.ad-showing")) { //this was also found on stackoverflow, link will also be linked in slideshow (and here too: https://stackoverflow.com/questions/63749340/on-a-youtube-video-page-how-do-i-check-if-the-video-is-currently-playing-an-ad)
-           mute();
+            gainNode.gain.value = 0;
         }
         else {
-            unmute();
+            gainNode.gain.value = 1;
         }
 }
+
+function skipAd() { //also found this function on stackoverflow, might work but need to see (link: https://stackoverflow.com/questions/79716754/how-can-i-reliably-click-youtube-s-skip-ad-button-via-content-script-in-a-chro)
+        const skipButtons = [
+            '.ytp-ad-skip-button-modern',
+            '.ytp-skip-ad-button',
+            'button[aria-label^="Skip ad"]'
+        ];
+
+        for (const selector of skipButtons) {
+            const button = document.querySelector(selector);
+            if (button && button.offsetParent !== null) {
+                button.click();
+                return;
+            }
+        }
+
+        // Seek through unskippable ads
+        const video = document.querySelector('video');
+        if (video && document.querySelector('.ad-showing, .ad-interrupting')) {
+            video.currentTime = video.duration - 0.1;
+        }
+    }
